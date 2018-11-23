d2698c4368d04d7dd3e736a77e498b9df3ced9ce
Raki <basnayrc@mail.uc.edu>
2018-11-23 11:04:49 -0500

    Test Suite Added

---
+++ test/server/service.test.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const should = require('should')
+const request = require('supertest')
+const service = require('../../server/service')
+
+describe('The express service', () => {
+  describe('GET/ foo', () => {
+    it('should return HTTP 404', done => {
+      request(service)
+        .get('/foo')
+        .expect(404, done)
+    })
+  })
+})
+
+describe('GET /service/:location', () => {
+  it('should return HTTP 200 with valid result', done => {
+    request(service)
+      .get('/service/Colombo')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        res.body.result.should.exist
+        return done()
+      })
+  })
+})
